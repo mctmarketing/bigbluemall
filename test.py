b8fadd0ad19b3d56b8dc5756caaec04c8700d952
mctmarketing <40162265+mctmarketing@users.noreply.github.com>
2018-07-13 16:03:28 +0700

    Update test.py

--- test.py
+++ test.py
@@ -77,29 +77,74 @@ def sendlocation(user):
 def sendCarousel(user):
   data = json.dumps({
   "replyToken":user,
-  "messages":[
-      {
-        "type": "imagemap",
-        "baseUrl": "https://www.picz.in.th/images/2018/07/11/NEd20v.jpg",
-        "altText": "This is an imagemap",
-        "baseSize": {
-            "height": 1040,
-            "width": 1040
-        },
-        "actions": [
-            {
-                "type": "uri",
-                "linkUri": "https://www.picz.in.th/images/2018/07/11/NEd20v.jpg",
-                "area": {
-                    "x": 240,
-                    "y": 240,
-                    "width": 500,
-                    "height": 980
-                }
-            }
-        ]
+  "messages":
+    [{
+      "type": "template",
+      "altText": "this is a carousel template",
+      "template": {
+          "type": "carousel",
+          "columns": [
+              {
+                "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+                "imageBackgroundColor": "#FFFFFF",
+                "title": "this is menu",
+                "text": "description",
+                "defaultAction": {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/123"
+                },
+                "actions": [
+                    {
+                        "type": "postback",
+                        "label": "Buy",
+                        "data": "action=buy&itemid=111"
+                    },
+                    {
+                        "type": "postback",
+                        "label": "Add to cart",
+                        "data": "action=add&itemid=111"
+                    },
+                    {
+                        "type": "uri",
+                        "label": "View detail",
+                        "uri": "http://example.com/page/111"
+                    }
+                ]
+              },
+              {
+                "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+                "imageBackgroundColor": "#000000",
+                "title": "this is menu",
+                "text": "description",
+                "defaultAction": {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/222"
+                },
+                "actions": [
+                    {
+                        "type": "postback",
+                        "label": "Buy",
+                        "data": "action=buy&itemid=222"
+                    },
+                    {
+                        "type": "postback",
+                        "label": "Add to cart",
+                        "data": "action=add&itemid=222"
+                    },
+                    {
+                        "type": "uri",
+                        "label": "View detail",
+                        "uri": "http://example.com/page/222"
+                    }
+                ]
+              }
+          ],
+          "imageAspectRatio": "rectangle",
+          "imageSize": "cover"
       }
-  ]
+    }]
   })
   r = requests.post(LINE_API, headers=headers, data=data)
   
